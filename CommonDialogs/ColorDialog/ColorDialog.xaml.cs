8834c36af22b063c20149d2b00d7be15b4d72351
Magicfork8459 <sjr1322@gmail.com>
2023-05-29 20:02:58 -0400

    WIP

--- CommonDialogs/ColorDialog/ColorDialog.xaml.cs
+++ CommonDialogs/ColorDialog/ColorDialog.xaml.cs
@@ -18,6 +18,14 @@ using System.Collections.Specialized;
 
 namespace Monkeyshines
 {
+    internal class ColorChangedEventArgs : EventArgs
+    {
+        public Color NewColor { get; set; }
+        public Color OldColor { get; set; }
+    }
+
+    internal delegate void ColorChangedEventHandler(object sender, ColorChangedEventArgs args);
+
     public partial class ColorDialog : Window
     {
         private static ValidationRuleHexCode validateHexCode = new ValidationRuleHexCode(6);
@@ -28,9 +36,9 @@ namespace Monkeyshines
         private Swatch? selectedSwatch = null;        
 
         public ObservableCollection<Swatch> CachedSwatches = new();
-        public string HexCode { get; set; }
 
-        Color currentColor = Colors.White;
+        Color currentColor;
+        private uint updates = 1;
 
         //! Constructor that takes in a Brush for Current
         public ColorDialog(SolidColorBrush current)
@@ -78,41 +86,90 @@ namespace Monkeyshines
                 swatch.Selected += Swatch_Selected;
             }
 
-            ColorUpdate(current.Color);
+            ColorUpdate(this, current.Color);
         }
 
-        private void ColorUpdate(Color color)
+
+        private void SliderValueUpdate(Color color)
         {
+            byte max = Math.Max(color.R, Math.Max(color.G, color.B));
+            byte brightness = (byte)(max / (ColorSliderRed.Maximum / ColorSliderBrightness.Maximum));
+
+            ColorSliderAlpha.Value = color.A;
+            ColorSliderRed.Value = color.R;
+            ColorSliderGreen.Value = color.G;
+            ColorSliderBlue.Value = color.B;
+           // ColorSliderBrightness.Value = brightness;
+        }
+
+        private void TextBoxTextUpdate(Color color)
+        {
+            byte max = Math.Max(color.R, Math.Max(color.G, color.B));
+            byte brightness = (byte)(max / (ColorSliderRed.Maximum / ColorSliderBrightness.Maximum));
+
+            TextBoxAlpha.Text = color.A.ToString();
+            TextBoxRed.Text = color.R.ToString();
+            TextBoxGreen.Text = color.G.ToString();
+            TextBoxBlue.Text = color.B.ToString();
+            TextBoxBrightness.Text = brightness.ToString();
+        }        
+
+        private void ColorUpdate(object updater, Color color)
+        {
+            TextBox? asTextbox = updater as TextBox;
+            //byte max = Math.Max(color.R, Math.Max(color.G, color.B));
+            //byte brightness = (byte)(max / (ColorSliderRed.Maximum / ColorSliderBrightness.Maximum));
             string colorCode = color.ToString();
             string alphaCode = colorCode.Substring(1, 2);
             string hexColorCode = colorCode.Substring(3);
-            
+
             TextBoxHexCode.Text = hexColorCode;
             TextBoxAlphaHexCode.Text = alphaCode;
 
-            ColorSliderAlpha.Resources["ControlBeginColor"] = Color.FromArgb((byte) ColorSliderAlpha.Minimum, color.R, color.G, color.B);
-            ColorSliderAlpha.Resources["ControlEndColor"]   = Color.FromArgb((byte) ColorSliderAlpha.Maximum, color.R, color.G, color.B);
+            ColorSliderAlpha.Resources["ControlBeginColor"] = Color.FromArgb((byte)ColorSliderAlpha.Minimum, color.R, color.G, color.B);
+            ColorSliderAlpha.Resources["ControlEndColor"] = Color.FromArgb((byte)ColorSliderAlpha.Maximum, color.R, color.G, color.B);
 
-            ColorSliderRed.Resources["ControlBeginColor"]   = Color.FromArgb(color.A, (byte) ColorSliderRed.Minimum, color.G, color.B);
-            ColorSliderRed.Resources["ControlEndColor"]     = Color.FromArgb(color.A, (byte) ColorSliderRed.Maximum, color.G, color.B);
-            
-            ColorSliderGreen.Resources["ControlBeginColor"] = Color.FromArgb(color.A, color.R, (byte) ColorSliderGreen.Minimum, color.B);
-            ColorSliderGreen.Resources["ControlEndColor"]   = Color.FromArgb(color.A, color.R, (byte) ColorSliderGreen.Maximum, color.B);
+            ColorSliderRed.Resources["ControlBeginColor"] = Color.FromArgb(color.A, (byte)ColorSliderRed.Minimum, color.G, color.B);
+            ColorSliderRed.Resources["ControlEndColor"] = Color.FromArgb(color.A, (byte)ColorSliderRed.Maximum, color.G, color.B);
 
-            ColorSliderBlue.Resources["ControlBeginColor"]  = Color.FromArgb(color.A, color.R, color.G, (byte) ColorSliderBlue.Minimum);
-            ColorSliderBlue.Resources["ControlEndColor"]    = Color.FromArgb(color.A, color.R, color.G, (byte) ColorSliderBlue.Maximum);
+            ColorSliderGreen.Resources["ControlBeginColor"] = Color.FromArgb(color.A, color.R, (byte)ColorSliderGreen.Minimum, color.B);
+            ColorSliderGreen.Resources["ControlEndColor"] = Color.FromArgb(color.A, color.R, (byte)ColorSliderGreen.Maximum, color.B);
 
-            //TODO Luminosity
-            //TODO slider textboxes
+            ColorSliderBlue.Resources["ControlBeginColor"] = Color.FromArgb(color.A, color.R, color.G, (byte)ColorSliderBlue.Minimum);
+            ColorSliderBlue.Resources["ControlEndColor"] = Color.FromArgb(color.A, color.R, color.G, (byte)ColorSliderBlue.Maximum);
 
-            ColorSliderAlpha.Value  = color.A;
-            ColorSliderRed.Value    = color.R;
-            ColorSliderGreen.Value  = color.G;
-            ColorSliderBlue.Value   = color.B;
-            
-            
+            //ColorSliderBrightness.Resources["ControlBeginColor"] = Color.FromArgb(color.A, (byte)ColorSliderRed.Minimum, (byte)ColorSliderGreen.Minimum, (byte)ColorSliderBlue.Minimum);
+            //ColorSliderBrightness.Resources["ControlEndColor"] = Color.FromArgb(color.A, (byte)ColorSliderRed.Value, (byte)ColorSliderGreen.Value, (byte)ColorSliderBlue.Value);
+
+            switch (updater)
+            {
+                case TextBox asTextBox:
+                    SliderValueUpdate(color);
+                    break;
+                case Slider asSlider:
+                    //if (ReferenceEquals(asSlider, ColorSliderBrightness))
+                    //{
+                    //    SliderValueUpdate(color);
+                    //    //ColorSliderBrightness.Value = brightness;
+                    //}
+                    //else
+                    //{
+                    //    TextBoxTextUpdate(color);
+                    //}
+                    TextBoxTextUpdate(color);
+                    break;
+                default:
+                    SliderValueUpdate(color);
+                    TextBoxTextUpdate(color);
+                    break;
+            }
+
             SwatchCurrent.Fill = new SolidColorBrush(color);
-            currentColor = color;
+            //Color.
+            //if (!ReferenceEquals(updater, ColorSliderBrightness))
+            { //! The point of the brightness is to get shades of the same color, so we have to maintain that color in memory
+                currentColor = color;
+            }
         }
         //! Constructor that takes in a Brush for Current and collection of Brushes for Cached
 
@@ -250,7 +307,7 @@ namespace Monkeyshines
             TextBox asTextBox = (TextBox)sender;
             bool validUpdate = false;
             string currentAlpha = string.IsNullOrEmpty(TextBoxAlphaHexCode.Text) ? "FF" : TextBoxAlphaHexCode.Text;
-            string currentColor = string.IsNullOrEmpty(TextBoxHexCode.Text) ? "FFFFFF" : TextBoxHexCode.Text;
+            string currentColorCode = string.IsNullOrEmpty(TextBoxHexCode.Text) ? "FFFFFF" : TextBoxHexCode.Text;
             
             if(ReferenceEquals(asTextBox, TextBoxAlphaHexCode))
             { // validate the alpha
@@ -264,26 +321,11 @@ namespace Monkeyshines
             // apply the new update if it was valid
             if(validUpdate)
             {
-                string colorString = $"#{ currentAlpha }{ currentColor }";
+                string colorString = $"#{ currentAlpha }{ currentColorCode }";
                 //! catch the FormatException?
                 SwatchCurrent.Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString(colorString));
                 SwatchCurrent.UpdateLayout();
             }
-        }
-
-        private void CodesChanged()
-        {
-            // get the current values of both text boxes
-        }
-
-        private void SliderValuesToSwatch()
-        {
-            string colorCode = Color.FromArgb((byte)ColorSliderAlpha.Value, (byte)ColorSliderRed.Value, (byte)ColorSliderGreen.Value, (byte)ColorSliderBlue.Value).ToString();
-            string alphaCode = colorCode.Substring(1, 2);
-            string hexCode = colorCode.Substring(3);
-
-            TextBoxAlphaHexCode.Text = alphaCode;
-            TextBoxHexCode.Text = hexCode;
         }
 
         private void CachedSwatches_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs args)
@@ -317,30 +359,30 @@ namespace Monkeyshines
         private void ColorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> args)
         {
             Color modifiedColor = Color.FromArgb((byte) ColorSliderAlpha.Value, (byte) ColorSliderRed.Value, (byte )ColorSliderGreen.Value, (byte) ColorSliderBlue.Value);
-            ColorUpdate(modifiedColor);
+            ColorUpdate(this, modifiedColor);
         }
         
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            TextBox asTextBox = sender as TextBox;
-            Panel panel = asTextBox.Parent as Panel;
+            //TextBox asTextBox = sender as TextBox;
+            //Panel panel = asTextBox.Parent as Panel;
 
-            if (!asTextBox.Text.Any())
-            {
-                asTextBox.Text = "0";
-                //asTextBox.CaretIndex = asTextBox.Text.Length;
-                //TODO Only do this if this changed from user input
-                asTextBox.SelectAll();
+            //if (!asTextBox.Text.Any())
+            //{
+            //    asTextBox.Text = "0";
+            //    //asTextBox.CaretIndex = asTextBox.Text.Length;
+            //    //TODO Only do this if this changed from user input
+            //    asTextBox.SelectAll();
 
-            }
+            //}
 
-            foreach (object child in panel.Children)
-            {
-                if(child is Slider asSlider)
-                {
-                    asSlider.Value = Convert.ToByte(asTextBox.Text);
-                }
-            }
+            //foreach (object child in panel.Children)
+            //{
+            //    if(child is Slider asSlider)
+            //    {
+            //        asSlider.Value = Convert.ToByte(asTextBox.Text);
+            //    }
+            //}
 
         }
 
@@ -365,6 +407,18 @@ namespace Monkeyshines
             }
         }
 
-        
+        private void ColorSliderBrightness_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //double multiplier   = Math.Round((byte) ColorSliderBrightness.Value * 0.01f, 2);
+            //byte modifiedRed    = (byte)(currentColor.R * multiplier);
+            //byte modifiedGreen  = (byte)(currentColor.G * multiplier);
+            //byte modifiedBlue   = (byte)(currentColor.B * multiplier);
+            //Color modifiedColor = Color.FromArgb((byte) ColorSliderAlpha.Value, modifiedRed, modifiedGreen, modifiedBlue);
+            //// Decrease highest RGB values by the percentage of the brightness
+            //ColorUpdate(sender, modifiedColor);
+
+            //// Deactivate CurrentColorSetter
+            //// Activate
+        }
     }
 }
